8908e72c5de4ee6f8bf805e64070158190b69cb8
Bashilor <dyrda.robin@outlook.fr>
2017-07-26 15:47:32 +0200

    Code cleanup

--- bot.py
+++ bot.py
@@ -5,18 +5,17 @@ import os
 updater = Updater(token='YOUR-BOT-TOKEN')
 dispatcher = updater.dispatcher
 
-def echo(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Did you just send me a photo ?!")
+def photo(bot, update):
+    bot.send_chat_action(chat_id=update.message.chat_id, action="typing")
     photo_file = bot.get_file(update.message.photo[-1].file_id)
     pw_uuid = str(uuid4())
     pw_uuid = pw_uuid.split('-')[4]
     photo_file.download(pw_uuid + ".jpg")
-    bot.send_message(chat_id=update.message.chat_id, text="Just saved your photo !")
-    os.rename(pw_uuid + ".jpg", "/var/www/pw.rdyrda.fr/" + pw_uuid + ".jpg")
-    bot.send_message(chat_id=update.message.chat_id, text="https://pw.rdyrda.fr/" + pw_uuid + ".jpg")
+    os.rename(pw_uuid + ".jpg", "/var/www/pw.rdyrda.fr/i/" + pw_uuid + ".jpg")
+    bot.send_message(chat_id=update.message.chat_id, text="https://pw.rdyrda.fr/i/" + pw_uuid + ".jpg")
 
-echo_handler = MessageHandler(Filters.photo, echo)
-dispatcher.add_handler(echo_handler)
+photo_handler = MessageHandler(Filters.photo, photo)
+dispatcher.add_handler(photo_handler)
 
 updater.start_polling()
 updater.idle()
